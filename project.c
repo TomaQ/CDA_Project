fe06bc72843d8ff76b3ddd889d1a2d03bc964a28
TomaQ <thomasiscooly@gmail.com>
2014-04-16 21:19:38 -0400

    initial commit

---
+++ project.c
@@ -0,0 +1,90 @@
+//Thomas Tavarez
+//4/11/2014
+
+#include "spimcore.h"
+
+
+/* ALU */
+/* 10 Points */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* instruction fetch */
+/* 10 Points */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+    if(PC%4 == 0) //checks if the memory address is word aligned
+    {
+        *instruction = Mem[PC >> 2];
+        if(*instruction < 8) //chekcs if the instruction is legal then return 0
+            return 0;
+        else
+            return 1; //the instruction is illegal and the program must halt
+    }
+
+    return 1;//the address was not memory aligned
+}
+
+
+/* instruction partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+
+}
+
+
+
+/* instruction decode */
+/* 15 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+    *extended_value = *extended_value + (offset << 16);
+}
+
+/* ALU operations */
+/* 10 Points */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+/* 10 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
