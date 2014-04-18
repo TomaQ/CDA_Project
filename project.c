7a10d4026296122e519f83513d221321f012a806
TomaQ <thomasiscooly@gmail.com>
2014-04-17 23:50:13 -0400

    added ALU operations

--- project.c
+++ project.c
@@ -8,6 +8,43 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
+    switch((int)ALUControl)
+    {
+        case 000:
+            *ALUresult = A +B;
+            break
+		case 001:
+			*ALUresult = A - B;
+			break;
+		case 010:
+			if((int)A < (int)B)
+                *ALUresult = 1;
+            else
+                *ALUresult = 0;
+            break;
+		case 011:
+			if(A < B)
+                ALUresult = 1;
+            else
+                ALUresult = 0;
+			break;
+		case 100:
+			*ALUresult = A & B;
+			break;
+		case 101:
+			*ALUresult = A | B;
+			break;
+		case 110:
+			*ALUresult = B << 16;
+			break;
+		case 111:
+			*ALUresult = ~A;
+    }
+
+    if(ALUresult == 0)
+        *Zero = 1;
+    else
+        *Zero = 0;
 
 }
 
